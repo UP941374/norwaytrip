2eef75bfab8a1f3dee4a9a1c66b3cccabd54e1ac
up941374 <up941374@myport.ac.uk>
2022-06-14 21:26:11 +0100

    gpx draw

--- index.js
+++ index.js
@@ -17,49 +17,65 @@ window.onload = function() {
     const fuelStatsMPG = document.querySelector('#fuelStatsMPG');
 
     const fuelings = [];
+    let totalKM = 0;
+    let totalL = 0;
 
     const fueling1 = {
         date: "11/06/2022",
         stationName: "BP",
-        stationLocation:"Andover, UK",
-        odo:153948,
+        stationLocation: "Andover, UK",
+        odo: 153948,
         liters: 81.46,
         cost: 159.96,
       };
-
     fuelings.push(fueling1);
 
+      const fueling2 = {
+        date: "14/06/2022",
+        stationName: "TOTAL",
+        stationLocation: "Den Haag, Netherlands",
+        odo: 154313,
+        liters: 73.06,
+        cost: 146.55,
+      };
+    fuelings.push(fueling2);    
+
     function calcfuelStatsKms() {
-        return 0
+        const start = 153948;
+        const end = fuelings[fuelings.length-1].odo;
+        const diff = (end - start) * 1.6;
+        totalKM = diff;
+        return diff
     };
 
     function calcfuelStatsLiters(){
         let liters = 0;
-        for (const fueling of fuelings) {
-            liters = liters + fueling.liters
+        for (let i = 0; i < fuelings.length - 1; i++) {
+            liters = liters + fuelings[i].liters
         }
+        totalL  = liters;
         return liters
     };
 
     function calcfuelStatsCost(){
         let cost = 0;
-        for (const fueling of fuelings) {
-            cost = cost + fueling.cost
+        for (let i = 0; i < fuelings.length - 1; i++) {
+            cost = cost + fuelings[i].cost
         }
         return cost
     };
 
     function calcfuelStatsMPG(){
-        return 0    
+        return totalL/totalKM*100   
     }
 
     fuelStatsKms.textContent = calcfuelStatsKms() + ' KMs';
-    fuelStatsLiters.textContent = calcfuelStatsLiters() + ' l';
-    fuelStatsCost.textContent = '£' + calcfuelStatsCost();
-    fuelStatsMPG.textContent = calcfuelStatsMPG() + ' l/100km';
+    fuelStatsLiters.textContent = calcfuelStatsLiters().toFixed(2) + ' l';
+    fuelStatsCost.textContent = '£' + calcfuelStatsCost().toFixed(2);
+    fuelStatsMPG.textContent = calcfuelStatsMPG().toFixed(1) + ' l/100km';
 
-    //map section
-    let map = L.map('map').setView([55, 5], 4);
+    //live location section
+    let map = L.map('map').setView([55, 5], 4);   
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -68,7 +84,32 @@ window.onload = function() {
 
     //camping 1
     //51.17796037807209, 3.1462270521345164
-    let marker = L.marker([51.177, 3.146]).addTo(map);  
+
+
+    //camping 2
+    //52.19073285923369, 4.834967289238963
+
+    let marker = L.marker([52.1907, 4.8349]).addTo(map);  
+
+    //tracks section
+
+    let maptracks = L.map('maptracks').setView([55, 5], 4);
+    console.log(maptracks);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '© OpenStreetMap'
+    }).addTo(maptracks);
+
+    const tracks = ['gpx/day1.gpx','gpx/day2.gpx'];
+
+    for (const tr of tracks) {
+        new L.GPX(tr, {async: true}).on('loaded', function(e) {
+            maptracks.fitBounds(e.target.getBounds());
+          }).addTo(maptracks);
+        
+    }
+
 
 
     //default landing page
